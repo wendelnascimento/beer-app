315919a396b786a133999775eabe5e1a991b9ada
Wendel Nascimento <wendel.nascimento2@outlook.com>
2020-01-30 15:50:14 -0300

    feat: creating Home screen

---
+++ src/screens/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Wrapper, Title } from './styles';
+
+const Home = () => (
+  <Wrapper>
+    <Title>Beer App</Title>
+  </Wrapper>
+);
+
+export default Home;
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+`;
