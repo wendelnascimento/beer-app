f866fb3ad7407fb45838a3b07778e065f71c198a
Wendel Nascimento <wendel.nascimento2@outlook.com>
2020-01-30 15:50:47 -0300

    feat: adding normalize, fonts and configuring react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+
+import Home from './screens/Home';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
 
 const App = () => (
-  <h1>Beer App</h1>
+  <>
+    <GlobalStyle />
+    <Router>
+      <Switch>
+        <Route component={Home} />
+      </Switch>
+    </Router>
+  </>
 );
 
 export default App;
