9de66dfc2d5e137ef2c405200d8acce002033ff2
Wendel Nascimento <wendel.nascimento2@outlook.com>
2020-01-31 23:08:17 -0300

    fix: mock for navigator.geolocation locally

--- jest.setup.js
+++ jest.setup.js
@@ -3,28 +3,4 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'jest-styled-components';
 import 'isomorphic-fetch';
 
-const mockGeolocation = {
-  getCurrentPosition: jest
-    .fn()
-    .mockImplementationOnce((success) => Promise.resolve(
-      success({
-        coords: {
-          latitude: 51.1,
-          longitude: 45.3,
-        },
-      }),
-    ))
-    .mockImplementationOnce((success) => Promise.resolve(
-      success({
-        coords: {
-          latitude: 51.1,
-          longitude: 45.3,
-        },
-      }),
-    )),
-  watchPosition: jest.fn(),
-};
-
-global.navigator.geolocation = mockGeolocation;
-
 configure({ adapter: new Adapter() });
--- src/screens/Home/__tests__/Home.spec.jsx
+++ src/screens/Home/__tests__/Home.spec.jsx
@@ -268,6 +268,22 @@ describe('<Home />', () => {
     ],
   };
 
+  beforeEach(() => {
+    const mockGeolocation = {
+      getCurrentPosition: jest.fn().mockImplementationOnce((success) => Promise.resolve(
+        success({
+          coords: {
+            latitude: 51.1,
+            longitude: 45.3,
+          },
+        }),
+      )),
+      watchPosition: jest.fn(),
+    };
+
+    global.navigator.geolocation = mockGeolocation;
+  });
+
   afterEach(fetchMock.reset);
 
   it('does render properly', () => {
