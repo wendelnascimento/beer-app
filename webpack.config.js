ab92373bc838dbc0884598a3330815e24556d816
Wendel Nascimento <wendel.nascimento2@outlook.com>
2020-02-03 14:51:55 -0300

    fix: change webpack output setting

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      template: './public/index.html',
+      template: './public/index.template.html',
     }),
   ],
   module: {
